d3818b8b8ddcd57bdd84d3a79b72339ad157a663
Arturo <aviramalevaza1@gmail.com>
2025-04-16 12:47:14 -0500

    Commit2.0

---
+++ app/Http/Controllers/OrmController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Apprentices;
+use App\Models\Computer;
+use Illuminate\Http\Request;
+
+class OrmController extends Controller
+{
+    public function consultas(){
+        
+      // $apprentice = Apprentices::find(2);
+       //return $apprentice->computer;
+
+       //return Apprentices::with('computer')->get(); 
+
+    }
+}
